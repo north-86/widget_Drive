21052a966e2b8535e130698cc744ee245a788ad1
northernman86 <aliaksandrprystauka@gmail.com>
2022-04-07 14:37:05 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Threading;
+
+namespace widget_Drive
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DispatcherTimer timer = new DispatcherTimer(TimeSpan.FromSeconds(1), DispatcherPriority.Normal, (object o, EventArgs ev) =>
+            {
+                DriveInfo[] drives = DriveInfo.GetDrives();
+                if (drives != null)
+                {
+                    dataGrid.ItemsSource = drives;
+                }
+            }, this.Dispatcher);
+            timer.Start();
+        }
+    }
+}
